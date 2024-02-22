3913328cd27cde9e5a2201b07f392c1498b4ac38
Yuri Santiago <yurisantiagx5@gmail.com>
2024-02-22 15:42:30 -0300

    update

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -23,37 +23,37 @@ export default function Navbar() {
           <img src="logoNaoAdicionada.png" className="h-12 w-auto ml-4" />
         </a>
         <div className="hidden md:flex space-x-4"> {/* Oculto em dispositivos menores que md (médio) */}
-  <a href="app/pages/aboutMe.js" className="text-white  opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all">About</a>
-  <a href="/contatos" className="text-white  opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all">Contacts</a>
-</div>
-        <div className="md:hidden"> {/* Visível apenas em dispositivos menores que md (médio) */}
+          <a href="app/pages/aboutMe.js" className="text-white opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all">About</a>
+          <a href="/contatos" className="text-white opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all">Contacts</a>
+        </div>
+        <div className="md:hidden flex items-center"> {/* Visível apenas em dispositivos menores que md (médio) */}
           {/* Ícone do menu para dispositivos móveis */}
           <button onClick={toggleMobileMenu} className="text-white hover:text-gray-300">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 8h16M4 12h16M4 16h16"
-            />
-          </svg>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 8h16M4 12h16M4 16h16"
+              />
+            </svg>
           </button>
         </div>
       </div>
       {/* Menu para dispositivos móveis */}
       {mobileMenuOpen && (
-        <div className="md:hidden bg-black bg-opacity-0">
-        <Link href="/aboutMe">
-  <a className="text-white opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all">About</a>
-</Link>
-<Link href="/contatos">
-  <a className="text-white opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all">Contacts</a>
-</Link>
+        <div className="md:hidden bg-black bg-opacity-0 flex flex-col items-end">
+          <Link href="/aboutMe">
+            <a className="text-white opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all block mb-2">About</a>
+          </Link>
+          <Link href="/contatos">
+            <a className="text-white opacity-75 text-sm hover:border-b hover:border-gray-300 transition-all block mb-2">Contacts</a>
+          </Link>
         </div>
       )}
     </nav>
