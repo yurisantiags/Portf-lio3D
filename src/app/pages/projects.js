41ff7c7499141eecb6060db3cf4c9a95da09b1bd
Yuri Santiago <yurisantiagx5@gmail.com>
2024-02-22 14:41:24 -0300

    update

--- src/app/pages/projects.js
+++ src/app/pages/projects.js
@@ -16,7 +16,7 @@ return(
       Here you can find some of my projects and solutions developed
       </h2>
     </div>
-      <div className=' mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0 '>
+      <div className=' mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
           <Soon/> 
           <Soon/>
           <Soon/> 
