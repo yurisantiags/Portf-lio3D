71e0f99f5838b3da3a9ba8391c29b915f150922f
Yuri Santiago <yurisantiagx5@gmail.com>
2024-02-29 13:05:15 -0300

    update

--- src/app/components/soon.js
+++ src/app/components/soon.js
@@ -8,7 +8,7 @@ export default function Soon(){
     AOS.init();
   }, [])
     return(
-        <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000" className=" mb-8 md:mb-32 flex rounded-lg  lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
+        <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000" className=" mb-8 md:mb-32 flex rounded-lg lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
       <a
         href=""
         className="group text-center pointer-events-none cursor-default rounded-lg bg-white bg-opacity-5 border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
