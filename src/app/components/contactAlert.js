9a00b249ae889c37f94088054185ceb0e75d48ac
Yuri Santiago <yurisantiagx5@gmail.com>
2024-03-07 15:28:38 -0300

    update

--- src/app/components/contactAlert.js
+++ src/app/components/contactAlert.js
@@ -1,37 +1,45 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 
-export default function AlertMessage(){
+const AlertMessage = () => {
     const [showAlert, setShowAlert] = useState(true);
+  
+    useEffect(() => {
+      // Verificar se é um dispositivo móvel
+      const isMobile = window.innerWidth <= 768;
+  
+      // Verificar se a mensagem já foi mostrada
+      const hasShownMessage = localStorage.getItem('hasShownMessage');
+  
+      if (isMobile && hasShownMessage) {
+        setShowAlert(false);
+      } else {
+        setShowAlert(true);
+        // Marcar que a mensagem já foi mostrada
+        localStorage.setItem('hasShownMessage', 'true');
+      }
+    }, []);
   
     const handleCloseAlert = () => {
       setShowAlert(false);
     };
   
-    useEffect(() => {
-      // Verifica se a mensagem já foi fechada antes
-      const isAlertClosed = localStorage.getItem('isAlertClosed');
-      if (isAlertClosed) {
-        setShowAlert(false);
-      }
-    }, []);
-  
-    const showAlertClass = showAlert ? 'block' : 'hidden';
-    const alertPositionClass = window.innerWidth < 640 ? 'fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' : 'fixed bottom-10 right-10';
-  
     return (
-      <div className={`${showAlertClass} ${alertPositionClass} bg-white bg-opacity-20 border border-white text-white opacity-75 px-4 py-3 rounded-md shadow-md`}>
-        <button onClick={handleCloseAlert} className="float-right focus:outline-none">
-          <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fillRule="evenodd"
-              d="M2.646 2.646a.5.5 0 01.708 0L10 9.293l6.646-6.647a.5.5 0 01.708.708L10.707 10l6.647 6.646a.5.5 0 01-.708.708L10 10.707l-6.646 6.647a.5.5 0 01-.708-.708L9.293 10 2.646 3.354a.5.5 0 010-.708z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <p className='text-center'>Interact with the page (envelope).</p>
-      </div>
+      showAlert && (
+        <div className="fixed bottom-10 right-10 bg-white bg-opacity-20 border border-white text-white opacity-75 px-4 py-3 rounded-md shadow-md">
+          <button onClick={handleCloseAlert} className="float-right focus:outline-none">
+            <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fillRule="evenodd"
+                d="M2.646 2.646a.5.5 0 01.708 0L10 9.293l6.646-6.647a.5.5 0 01.708.708L10.707 10l6.647 6.646a.5.5 0 01-.708.708L10 10.707l-6.646 6.647a.5.5 0 01-.708-.708L9.293 10 2.646 3.354a.5.5 0 010-.708z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <p className='text-center'>Interact with the page (envelope).</p>
+        </div>
+      )
     );
   };
   
+  export default AlertMessage;
--- src/app/components/scene.js
+++ src/app/components/scene.js
@@ -9,113 +9,95 @@ export default function ModelViewer() {
   const sceneRef = useRef();
   const rendererRef = useRef();
   const modelRef = useRef();
-  const scaleDirectionRef = useRef(1); // Referência para controlar a direção da escala
-  const controlsRef = useRef(null); // Inicializamos com null para evitar erros de undefined
+  const scaleDirectionRef = useRef(1);
+  const controlsRef = useRef(null);
 
   useEffect(() => {
-    // Criação da cena
-    const scene = new THREE.Scene();
+    if (typeof window !== 'undefined') {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera.position.z = 2.5;
+      const renderer = new THREE.WebGLRenderer({ alpha: true });
+      const width = window.innerWidth * 0.7;
+      const height = window.innerHeight * 0.7;
+      renderer.setSize(width, height);
+      rendererRef.current = renderer;
+      sceneRef.current.appendChild(renderer.domElement);
 
-    // Configuração da câmera
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 2.5;
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+      scene.add(ambientLight);
 
-    // Configuração do renderer com dimensões personalizadas
-    const renderer = new THREE.WebGLRenderer({ alpha: true });
-    const width = window.innerWidth * 0.7; // Definindo a largura do renderizador como 70% da largura da janela
-    const height = window.innerHeight * 0.7; // Definindo a altura do renderizador como 70% da altura da janela
-    renderer.setSize(width, height);
-    rendererRef.current = renderer;
-    sceneRef.current.appendChild(renderer.domElement);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+      directionalLight.position.set(0, 10, 10);
+      directionalLight.castShadow = true;
+      scene.add(directionalLight);
 
-    // Configuração da luz ambiente
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
-    scene.add(ambientLight);
+      const loader = new GLTFLoader();
+      loader.load(
+        'img/scene-16.gltf',
+        (gltf) => {
+          gltf.scene.traverse((child) => {
+            if (child.isMesh) {
+              child.material.flatShading = false;
+              child.castShadow = true;
+              child.receiveShadow = true;
+            }
+          });
 
-    // Configuração da luz direcional para sombras
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-    directionalLight.position.set(0, 10, 10);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
+          gltf.scene.scale.set(1.0, 1.0, 1.0);
+          modelRef.current = gltf.scene;
+          scene.add(gltf.scene);
 
-    // Carregamento do modelo GLTF
-    const loader = new GLTFLoader();
-    loader.load(
-      'img/scene-16.gltf',
-      (gltf) => {
-        // Configuração do sombreamento suave para o material do modelo
-        gltf.scene.traverse((child) => {
-          if (child.isMesh) {
-            child.material.flatShading = false;
-            child.castShadow = true;
-            child.receiveShadow = true;
-          }
-        });
+          const controls = new OrbitControls(camera, renderer.domElement);
+          controlsRef.current = controls;
+          controls.enableDamping = true;
+          controls.dampingFactor = 0.25;
 
-        // Ajuste de escala inicial do modelo
-        gltf.scene.scale.set(1.0, 1.0, 1.0);
+          function animateModel() {
+            const scaleSpeed = 0.005;
+            modelRef.current.scale.x += scaleSpeed * scaleDirectionRef.current;
+            modelRef.current.scale.y += scaleSpeed * scaleDirectionRef.current;
+            modelRef.current.scale.z += scaleSpeed * scaleDirectionRef.current;
 
-        modelRef.current = gltf.scene;
-        scene.add(gltf.scene);
+            if (modelRef.current.scale.x <= 0.5 || modelRef.current.scale.x >= 1.5) {
+              scaleDirectionRef.current *= -1;
+            }
 
-        // Adiciona controles de órbita
-        const controls = new OrbitControls(camera, renderer.domElement);
-        controlsRef.current = controls;
-        controls.enableDamping = true; // Adiciona amortecimento para suavizar os movimentos
-        controls.dampingFactor = 0.25; // Ajusta o fator de amortecimento para controlar a suavidade dos movimentos
+            const rotationSpeed = 0.01;
+            modelRef.current.rotation.y += rotationSpeed;
 
-        // Função de animação de escala com rotação
-        function animateModel() {
-          // Ajuste da velocidade de escala
-          const scaleSpeed = 0.005; // Ajuste esse valor para alterar a velocidade de escala
-          modelRef.current.scale.x += scaleSpeed * scaleDirectionRef.current;
-          modelRef.current.scale.y += scaleSpeed * scaleDirectionRef.current;
-          modelRef.current.scale.z += scaleSpeed * scaleDirectionRef.current;
-
-          // Inverte a direção da escala quando atinge os limites
-          if (modelRef.current.scale.x <= 0.5 || modelRef.current.scale.x >= 1.5) {
-            scaleDirectionRef.current *= -1;
+            renderer.render(scene, camera);
+            requestAnimationFrame(animateModel);
           }
 
-          // Ajuste da velocidade de rotação
-          const rotationSpeed = 0.01; // Ajuste esse valor para alterar a velocidade de rotação
-          modelRef.current.rotation.y += rotationSpeed;
-
-          renderer.render(scene, camera);
-          requestAnimationFrame(animateModel);
+          animateModel();
+        },
+        undefined,
+        (error) => {
+          console.error('Erro ao carregar o modelo GLTF', error);
         }
+      );
 
-        animateModel();
-      },
-      undefined,
-      (error) => {
-        console.error('Erro ao carregar o modelo GLTF', error);
+      function handleResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth * 0.7, window.innerHeight * 0.7);
       }
-    );
 
-    // Função para atualizar o tamanho do renderer quando a janela é redimensionada
-    function handleResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth * 0.7, window.innerHeight * 0.7);
+      window.addEventListener('resize', handleResize);
+
+      renderer.domElement.style.cursor = 'pointer';
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+        if (controlsRef.current) {
+          controlsRef.current.dispose();
+        }
+        rendererRef.current.dispose();
+        sceneRef.current.removeChild(rendererRef.current.domElement);
+      };
     }
-
-    window.addEventListener('resize', handleResize);
-
-    // Definindo o cursor para indicar que o usuário pode interagir
-    renderer.domElement.style.cursor = 'pointer';
-
-    // Limpeza dos objetos criados quando o componente é desmontado
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      if (controlsRef.current) {
-        controlsRef.current.dispose(); // Dispose dos controles de órbita
-      }
-      rendererRef.current.dispose();
-      sceneRef.current.removeChild(rendererRef.current.domElement);
-    };
   }, []);
 
   return <div ref={sceneRef} />;
 }
-
