8cafadfb527fffb79f51c68b88b5a2a669c7fa6e
Yuri Santiago <yurisantiagx5@gmail.com>
2024-03-07 15:34:57 -0300

    update

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -5,25 +5,33 @@ import EmailBox from "../components/email";
 import LkButton from "../components/LkButton";
 import Footer from "../components/footer";
 import AlertMessage from "../components/contactAlert";
+import Building from "../components/building";
 
 export default function Contacts() {
   return (
-  <section >
-    <Navbar/>
-    <div className='flex min-h-screen items-center justify-center ' >
-    <div className='min-h-screen justify-center flex items-center flex-col'>
-      <ModelViewer />
-      <EmailBox/>
-     <div className="flex mt-8 space-x-8">
-        <GhButton/>
-        <LkButton/>
+//   <section >
+//     <Navbar/>
+//     <div className='flex min-h-screen items-center justify-center ' >
+//     <div className='min-h-screen justify-center flex items-center flex-col'>
+//       <ModelViewer />
+//       <EmailBox/>
+//      <div className="flex mt-8 space-x-8">
+//         <GhButton/>
+//         <LkButton/>
+//       </div>
+// </div>
+// </div>
+// <Footer/>
+// <AlertMessage/>
+// </section>  
+<main>
+    <div className='h-full w-full'>
+      <Navbar/>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Building/>
       </div>
-</div>
-</div>
-<Footer/>
-<AlertMessage/>
-</section>  
-
+    </div>
+</main>
   )
 }
 
