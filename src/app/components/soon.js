c2cbb59a7900d12f94b504fdfad53996cfd0dbbe
Yuri Santiago <yurisantiagx5@gmail.com>
2024-03-22 16:21:55 -0300

    update

--- src/app/components/soon.js
+++ src/app/components/soon.js
@@ -13,14 +13,14 @@ export default function Soon() {
   return (
     <div
       ref={squareRef}
-      className="mb-8 md:mb-32 flex w-full h-auto  rounded-lg lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left bg-white bg-opacity-5 justify-center items-center"
+      className="mb-8 md:mb-32 flex  rounded-lg lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left bg-white bg-opacity-5 justify-center items-center"
     >
       <a
         href=""
-        className="group text-center pointer-events-none cursor-default rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-center items-center"
+        className="group text-center md:w-[580px] md:h-[280px] lg:w-[580px] lg:h-[280px] lg:h-280 pointer-events-none cursor-default rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-center items-center"
         target="_blank"
         rel="noopener noreferrer"
-        style={{ width: "580px", height: "280px" }} // Manter o tamanho apenas em telas grandes
+         // Manter o tamanho apenas em telas grandes
       >
         <div>
           <h2 className={`text-3xl opacity-40 font-semibold text-gray-400`}>
