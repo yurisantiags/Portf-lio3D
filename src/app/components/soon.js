d911f640fa47e80b204cc3f108b6b098ad23e865
Yuri Santiago <yurisantiagx5@gmail.com>
2024-03-22 16:13:41 -0300

    update

--- src/app/components/soon.js
+++ src/app/components/soon.js
@@ -1,29 +1,36 @@
 'use client';
-import React,{useEffect} from "react";
+import React, { useEffect, useRef } from "react";
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 
-export default function Soon(){
+export default function Soon() {
   useEffect(() => {
     AOS.init();
-  }, [])
-    return(
-        <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000" className=" mb-8 md:mb-32 flex rounded-lg lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
+  }, []);
+
+  const squareRef = useRef(null);
+
+  return (
+    <div
+      ref={squareRef}
+      className="mb-8 md:mb-32 flex w-full h-auto  rounded-lg lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left bg-white bg-opacity-5 justify-center items-center"
+    >
       <a
         href=""
-        className="group text-center pointer-events-none cursor-default rounded-lg bg-white bg-opacity-5 border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        className="group text-center pointer-events-none cursor-default rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-center items-center"
         target="_blank"
         rel="noopener noreferrer"
+        style={{ width: "580px", height: "280px" }} // Manter o tamanho apenas em telas grandes
       >
-      <h2 className={`rounded-md opacity-40 text-2xl text-gray-400 font-semibold`}>
-          Soon{' '}
-        </h2>
-        <p className={`m-0 max-w-[30ch] text-sm opacity-40 text-gray-400`}>
-        This field is intended for future projects
-        </p>
+        <div>
+          <h2 className={`text-3xl opacity-40 font-semibold text-gray-400`}>
+            Soon
+          </h2>
+          <p className={`text-sm opacity-40 text-gray-400 mt-2`}>
+            This field is intended for future projects
+          </p>
+        </div>
       </a>
     </div>
-    )
+  );
 }
-
-
