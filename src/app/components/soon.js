fe9d89ad0380267639a74057d98cc0986bd17166
Yuri Santiago <yurisantiagx5@gmail.com>
2024-03-27 14:53:51 -0300

    update

--- src/app/components/soon.js
+++ src/app/components/soon.js
@@ -25,12 +25,12 @@ export default function Soon() {
   }, [projects.length]); // Dispara o efeito sempre que o número de projetos mudar
 
   return (
-    <div className="relative h-screen w-screen bg-white bg-opacity-5 flex justify-center items-center overflow-hidden">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"> {/* Centraliza o carousel */}
+    <div className="relative h-screen text-center w-screen bg-white bg-opacity-5 flex justify-center items-center overflow-hidden">
+      <div className="absolute top-1/2 text-center left-1/2 transform -translate-x-1/2 -translate-y-1/2"> {/* Centraliza o carousel */}
         {projects.map((project, index) => (
           <div
             key={index}
-            className={`group text-center w-3/4 h-3/4 pointer-events-none cursor-default rounded-lg border border-transparent px-5 py-4 transition-all hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-center items-center absolute top-0 left-0`}
+            className={`group w-3/4 h-3/4 pointer-events-none cursor-default rounded-lg border border-transparent transition-all hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex justify-center items-center absolute top-0 left-0`}
             style={{
               transform: `rotateX(${currentIndex === index ? 0 : 90}deg) translateY(${currentIndex === index ? 0 : index > currentIndex ? "100%" : "-100%"})`,
               opacity: index === currentIndex ? 1 : 0,
