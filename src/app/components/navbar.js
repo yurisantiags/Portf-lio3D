7674f1fb355c5ef19d77c66f23bd03a91dd9b2fa
Yuri Santiago <yurisantiagx5@gmail.com>
2024-04-01 11:58:20 -0300

    update

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -19,8 +19,8 @@ export default function Navbar() {
   return (
     <nav className="bg-black bg-opacity-0 fixed w-full top-0 z-10">
       <div className="container mx-auto flex items-center justify-between">
-        <a href='#' className='hidden md:block' onClick={scrollToTop}>
-          <img src="img/clover.png" className="h-7 mt-3 w-auto ml-4" />
+        <a href='#' className='' onClick={scrollToTop}>
+          <img src="img/clover.png" className="hidden md:block h-7 mt-3 w-auto ml-4" />
         </a>
         <div className="hidden md:flex space-x-4"> {/* Oculto em dispositivos menores que md (médio) */}
           <Link href="/home" className="text-white opacity-75 text-sm hover:underline transition-all">Home</Link>
