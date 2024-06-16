1862ed80ba36f2b8047d5ab45e37aab8ab4dbe81
Yuri Santiago <yurisantiagx5@gmail.com>
2024-06-15 22:57:52 -0300

    update

--- src/app/components/aboutImage.js
+++ src/app/components/aboutImage.js
@@ -1,20 +1,21 @@
 'use client';
-import React from 'react';
 import { useMediaQuery } from 'react-responsive';
-  
+
 function Imagem(props) {
   const isDesktop = useMediaQuery({ minWidth: 769 }); // Define a largura mínima para considerar como tela desktop
 
   if (!isDesktop) {
-    return null; // Retorna null se não estiver em uma tela desktop (ou seja, está em uma tela de notebook)
+    return null; // Retorna null se não estiver em uma tela desktop
   }
+
+  const imagePath = '/img/perf-2.png'; // Substitua 'sua-imagem.jpg' pelo nome da sua imagem
 
   return (
     <div className=''>
       <img
         data-aos="zoom-in-up"
         className='w-64 h-72 rounded-xl opacity-0 md:opacity-100 border-r-2 border-b-2 border-r-gray-600 border-b-gray-600'
-        src={props.src}
+        src={imagePath}
         alt={props.alt}
       />
     </div>
