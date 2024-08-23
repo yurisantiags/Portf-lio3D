555e4050b221ffd12a4bb93730e08a186021493a
Yuri Santiago <yurisantiagx5@gmail.com>
2024-08-23 14:32:51 -0300

    Project Finalized (subject to future improvements)

--- src/app/components/aboutTxt.js
+++ src/app/components/aboutTxt.js
@@ -37,7 +37,7 @@ export default function AboutTxt() {
         <h2 className="font-semibold text-black text-2xl mb-4 animated-heading">About Me</h2>
         <div className="mt-4">
           <h4 className="font-semibold text-lg mt-4">Expertise/Skills:</h4>
-          <ul className="list-disc pl-5">
+          <ul className=" pl-5">
             <li>Front-end: Proficient in Bootstrap, Tailwind, HTML, CSS, SASS, JavaScript, PHP, Next.js, and React.</li>
             <li>Design: Comprehensive experience in designing and implementing complete websites, including 3D modeling and animations (Three.js).</li>
             <li>Design Tools: Figma, Photoshop, Canva, and Blender.</li>
@@ -45,7 +45,7 @@ export default function AboutTxt() {
             <li>Cyber Security: Knowledge in vulnerability analysis and data protection using Python.</li>
           </ul>
           <h4 className="font-semibold text-lg mt-4">Experience:</h4>
-          <ul className="list-disc pl-5">
+          <ul className=" pl-5">
             <li>Development and maintenance of corporate websites.</li>
             <li>Design and development of front-end interfaces.</li>
             <li>Creation of 3D models and advanced visual elements.</li>
