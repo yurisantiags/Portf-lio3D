a97659a9ecde89c6285c3a656c9caf4387e40d5b
Yuri Santiago <yurisantiagx5@gmail.com>
2024-08-25 20:15:59 -0300

    Fix: Img

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,5 @@
 'use client '
-import Imagem from '../components/aboutImage';
+import Image from '../components/aboutImage';
 import TimeLine from '../components/timeline';
 
 export default function About() {
@@ -7,7 +7,7 @@ export default function About() {
     <main id='about' className="h-screen mt-72 flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center">
         <div className="flex justify-center md:pl-24 md:pb-14">
-          <Imagem src="" alt="me"/>
+          <Image src="" alt="me"/>
          <TimeLine/>
         </div>
       </div>
