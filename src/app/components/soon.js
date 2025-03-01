194cccee01c5f7f8d0d9f12e22f0d1a28e5ae5d2
Yuri Santiago <yurisantiagx5@gmail.com>
2025-03-01 19:58:20 -0300

    update

--- src/app/components/soon.js
+++ src/app/components/soon.js
@@ -15,7 +15,7 @@ export default function Soon() {
       data-aos-duration="2000" 
       className="justify-center items-center flex w-80 rounded-lg"
     >
-      <div className="relative mt-16 hover:scale-105 text-center transition-opacity duration-200 mx-10 md:mx-0 justify-center items-center flex h-96 bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden">
+      <div className="relative mt-16 text-center transition-opacity duration-200 mx-10 md:mx-0 justify-center items-center flex h-96 bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden">
         
         <div className="absolute top-4 left-4 text-left">
           <h2 className="text-xl md:text-2xl font-semibold text-black opacity-90 font-sans tracking-wide">
